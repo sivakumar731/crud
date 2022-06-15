fba7b1840360dfa6b6202bd789b47aa0fcd78738
sivakumar <bsivakumar1609@gmail.com>
2022-06-15 14:13:05 +0530

    simple form validation
    Signed-off-by: sivakumar <bsivakumar1609@gmail.com>

--- gmail/src/App.js
+++ gmail/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from 'react';
+// import axios from "axios"
+
+// import 
+
+Signup from './task/Signup';
 
 function App() {
+ 
+  const [data,setData]=useState({
+    username:"",
+    password:"",
+
+  })
+  const{username,password}=data;
+  const handleChange=(e)=>{
+   setData({...data,[e.target.name]:e.target.value})
+    
+  }
+  
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+
+    if(data.username.length<5){
+alert("username should be mmore than 5 characters")
+    }else
+    
+    {console.log(data)}
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+     <form action="submit" onSubmit={handleSubmit}>
+       <label htmlFor="username">username</label>
+       <input type="text" name='username' value={username}  onChange={handleChange} />
+       <label htmlFor="password">password</label>
+       <input type="password" name="password" value={password} onChange={handleChange} />
+     <button >submit</button>
 
+     </form>
+
+
+
+
+
+{/* <Login /> */}
+{/* <Signup /> */}
+
+
+    </div>
+
+  )
+  }
 export default App;
